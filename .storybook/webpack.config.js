357ca9be3a123184a21d78a0edbc274c6bdb57df
Alf Eaton <eaton.alf@gmail.com>
2020-02-27 14:19:41 +0000

    Upgrade Storybook and Jest

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,22 @@
-const path = require('path');
+module.exports = ({ config }) => {
+  config.module.rules[0].use[0].options.presets = [
+    require.resolve('@babel/preset-env'),
+    require.resolve('@babel/preset-react'),
+    require.resolve('@babel/preset-typescript'),
+  ]
 
-module.exports = (baseConfig, env, config) => {
   config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    use: [{
-      loader: require.resolve('babel-loader')
-    }]
-  });
-  config.resolve.extensions.push('.ts', '.tsx');
+    exclude: /node_modules/,
+    test: /\.tsx?$/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        cacheDirectory: true,
+      },
+    },
+  })
+
+  config.resolve.extensions.push('.ts', '.tsx')
+
   return config;
 };
