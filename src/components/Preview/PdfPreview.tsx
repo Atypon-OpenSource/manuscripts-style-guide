e200399636136dde6cd6bcd22673601a55200471
Ahmad Souqi <asouqi@atypon.com>
2020-10-19 12:01:27 +0000

    LW  - PDF Preview: -> declare pdf-js worker and viewer -> use nodRef to get container -> use styled component for storybook

---
+++ src/components/Preview/PdfPreview.tsx
@@ -0,0 +1,67 @@
+/*!
+ * © 2020 Atypon Systems LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import 'pdfjs-dist/web/pdf_viewer.css'
+
+import {
+  getDocument,
+  GlobalWorkerOptions,
+  PDFDocumentProxy,
+  PDFPromise,
+} from 'pdfjs-dist'
+import * as pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry'
+import { EventBus, PDFViewer } from 'pdfjs-dist/web/pdf_viewer'
+import React, { useEffect, useRef, useState } from 'react'
+
+GlobalWorkerOptions.workerSrc = pdfjsWorker
+
+const PdfPreview: React.FC<{ scale?: number; url: string }> = ({
+  url,
+  scale = 1,
+}) => {
+  const [pdfViewer, setPdfViewer] = useState<{ currentScaleValue: number }>({
+    currentScaleValue: scale,
+  })
+  const nodeRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const container = nodeRef.current
+    const eventBus = new EventBus()
+
+    const pdfViewer = new PDFViewer({
+      container,
+      eventBus,
+    })
+    setPdfViewer(pdfViewer)
+
+    eventBus.on('pagesinit', () => (pdfViewer.currentScaleValue = scale))
+    const loadingTask = getDocument(url)
+    const proxyPDFPromise: PDFPromise<PDFDocumentProxy> = loadingTask.promise.then(
+      (pdfDocument) => pdfViewer.setDocument(pdfDocument)
+    )
+  }, [url])
+
+  useEffect(() => {
+    pdfViewer.currentScaleValue = scale
+  }, [pdfViewer, scale])
+
+  return (
+    <div ref={nodeRef} id={'viewerContainer'}>
+      <div id="viewer" className="pdfViewer" />
+    </div>
+  )
+}
+
+export default PdfPreview
