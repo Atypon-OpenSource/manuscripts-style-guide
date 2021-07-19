643e25e3a3a745e65d198bf8d93f303b472ae575
@CAYdenberg <ydenberg@gmail.com>
2021-07-19 16:53:40 -0700

    ADD save status indicator (MAN-854)

---
+++ src/components/SaveStatus.tsx
@@ -0,0 +1,128 @@
+/*!
+ * © 2020 Atypon Systems LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react'
+import styled from 'styled-components'
+
+import SaveIconOffline from './icons/save-icon-offline'
+import SaveIconSaved from './icons/save-icon-saved'
+import SaveIconSaving from './icons/save-icon-saving'
+
+interface Props {
+  status: 'saving' | 'saved' | 'offline'
+}
+
+export const SaveStatus: React.FC<Props> = ({ status }) => {
+  switch (status) {
+    case 'offline':
+      return (
+        <Container>
+          <SaveIconOffline />
+          <Text>Offline</Text>
+        </Container>
+      )
+
+    case 'saving':
+      return (
+        <Container>
+          <RotateContinuous>
+            <SaveIconSaving />
+          </RotateContinuous>
+          <Text>Saving&hellip;</Text>
+        </Container>
+      )
+
+    case 'saved':
+      return (
+        <Container>
+          <PopPop>
+            <SaveIconSaved />
+          </PopPop>
+          <Text>Saved</Text>
+        </Container>
+      )
+  }
+}
+
+const Container = styled.div`
+  color: ${(props) => props.theme.colors.text.secondary};
+  display: inline-flex;
+  flex-direction: row;
+  align-items: center;
+
+  path {
+    stroke: ${(props) => props.theme.colors.text.secondary};
+  }
+`
+
+const Text = styled.span`
+  margin-left: 0.5em;
+  font-family: ${(props) => props.theme.font.family.Lato};
+  font-size: 14px;
+  font-weight: 700;
+`
+
+const RotateContinuous = styled.div`
+  svg {
+    animation: saveRotation 2.7s ease-in-out infinite;
+    @media (prefers-reduced-motion) {
+      animation: unset;
+    }
+  }
+
+  @keyframes saveRotation {
+    0% {
+      transform: rotate(0);
+    }
+
+    20% {
+      transform: rotate(180deg);
+    }
+
+    50% {
+      transform: rotate(180deg);
+    }
+
+    70% {
+      transform: rotate(360deg);
+    }
+
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`
+
+const PopPop = styled.div`
+  svg {
+    animation: PopPop 0.3s ease-in-out 2;
+    @media (prefers-reduced-motion) {
+      animation: unset;
+    }
+  }
+
+  @keyframes PopPop {
+    0% {
+      transform: scale(1);
+    }
+    50% {
+      transform: scale(1.2);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+`
---
+++ src/components/icons/save-icon-offline.tsx
@@ -0,0 +1,48 @@
+/*!
+ * © 2019 Atypon Systems LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React from 'react'
+
+const SaveIconOffline: React.FC = () => (
+  <svg
+    width={18}
+    height={18}
+    viewBox="0 0 18 18"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M12.4878 5.51279C11.862 4.67021 10.9863 4.04663 9.98537 3.73091C8.98446 3.41518 7.90943 3.42342 6.91347 3.75445C5.91751 4.08547 5.05144 4.7224 4.43866 5.57446C3.82588 6.42653 3.49767 7.45026 3.50078 8.49979C3.50078 8.51679 3.50078 8.53279 3.50078 8.54979C2.8932 8.67316 2.35314 9.01789 1.98544 9.51705C1.61773 10.0162 1.44862 10.6342 1.51092 11.251C1.57323 11.8679 1.86251 12.4396 2.32261 12.8551C2.78271 13.2706 3.3808 13.5004 4.00078 13.4998H4.50078"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M7.32877 13.5003H14.0008C14.6638 13.5003 15.2997 13.2369 15.7685 12.7681C16.2374 12.2992 16.5008 11.6634 16.5008 11.0003C16.5008 10.3373 16.2374 9.70139 15.7685 9.23255C15.2997 8.7637 14.6638 8.50031 14.0008 8.50031C13.8312 8.50044 13.662 8.51752 13.4958 8.55131C13.4958 8.53331 13.5008 8.51731 13.5008 8.50031C13.4995 8.14553 13.4606 7.79189 13.3848 7.44531"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M1.50079 16.5L16.5008 1.5"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
+
+export default SaveIconOffline
