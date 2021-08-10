b48d15a861051fe8bd0a9c063debe6457ea88bd2
Mikhail Bartenev <mbartenev@atypon.com>
2021-08-10 16:32:01 +0000

    Error messages for the LW error codes LEAN-87

---
+++ src/lib/lw-errors-decoder.ts
@@ -0,0 +1,139 @@
+/*!
+ * © 2020 Atypon Systems LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+interface errorDescription {
+  title: string
+  description: string
+}
+
+interface errorsAssocList {
+  [key: string]: errorDescription
+}
+
+const errorsExplanations: errorsAssocList = {
+  ANY_ERR_AUTHORIZATION: {
+    title: 'Authorization issue',
+    description: "It seems you don't have access to this page. If you think...",
+  },
+  PREVIEW_HTML_GENERATION_FAILED: {
+    title: 'Failure in auto-generation of HTML preview',
+    description:
+      'The HTML preview could not be generated due to an internal error.',
+  },
+  PREVIEW_PDF_GENERATION_FAILED: {
+    title: 'Failure in auto-generation of PDF preview',
+    description:
+      'The PDF preview could not be generated due to an internal error.',
+  },
+  PREVIEW_PDF_TEMPLATE_MISSING: {
+    title: 'Failure in auto-generation of PDF preview',
+    description:
+      'The PDF preview could not be generated due to an internal error.',
+  },
+  PREVIEW_EPUB_GENERATION_FAILED: {
+    title: 'Failure in auto-generation of ePub preview',
+    description:
+      'The ePub preview could not be generated due to an internal error.',
+  },
+  PREVIEW_SERVICE_UNAVAILABLE: {
+    title: 'The preview service is unavailable',
+    description: 'The preview could not be generated due to an internal error.',
+  },
+  MANUSCRIPT_CONTENT_PARSING_FAILED: {
+    title: 'Quality report cannot be generated',
+    description: 'There is a problem generating the Quality report.',
+  },
+  QR_PROFILE_NOT_FOUND: {
+    title: 'Quality service configuration error',
+    description: 'The Quallity service is not cofigured correctly.',
+  },
+  QR_SERVICE_UNAVAILABLE: {
+    title: 'Quality service unavailable',
+    description: 'The Quallity service is not available at the moment.',
+  },
+  MANUSCRIPT_ARCHIVE_FETCH_FAILED: {
+    title: 'Content cannot be loaded',
+    description: 'There was an error trying to load the content.',
+  },
+  EDT_QAREPORT_INVALID: {
+    title: 'Invalid Quality report',
+    description: 'The quality report could not be loaded.',
+  },
+  TSK_MYTASKS_NOT_LOADED: {
+    title: 'Task list cannot be loaded',
+    description:
+      'There was an internal error while loading the list of your tasks.',
+  },
+  TSK_TASKSQUEUE_NOT_LOADED: {
+    title: 'Task list cannot be loaded',
+    description:
+      'There was an internal error while loading the list of open tasks to pick up from.',
+  },
+  TSK_ASSIGN_FAILED: {
+    title: 'Task not assigned',
+    description:
+      'There was an internal error while assigning the task to your task list.',
+  },
+  NOTES_FETCH_FAILED: {
+    title: 'Production Notes cannot be loaded',
+    description:
+      'There was an internal error while loading the Production notes.',
+  },
+  NOTES_NOT_UPDATED: {
+    title: 'Production Notes cannot be updated',
+    description:
+      'There was an internal error while updating the Production notes.',
+  },
+  FHC_DOWNLOAD_FILE_NOT_EXISTS: {
+    title: 'File does not exist',
+    description:
+      'The file you have tried to download does not exist. Please contact tne support team.',
+  },
+  FHC_DOWNLOAD_FILE_NAME_NOT_EXISTS: {
+    title: 'Filename cannot be found',
+    description: '',
+  },
+  FHC_UPLOAD_FAIL: {
+    title: 'Upload of the file failed',
+    description: 'There was an internal error while uploading the file.',
+  },
+  WFM_AVAILABLE_TRANSITIONS_NOT_LOADED: {
+    title: 'Workflow steps not loaded',
+    description:
+      'There was an internal error while loading the possible workflow next steps.',
+  },
+  WFM_TRANSITION_FAIL: {
+    title: 'Workflow not advanced to next step',
+    description:
+      'There was an internal error while advancing to the next step of the workflow.',
+  },
+  SBM_RESCHEDULE_FAILED: {
+    title: 'Rescheduling the article failed',
+    description:
+      'There was an internal error while attempting to reschedule the due date of the article.',
+  },
+  MANUSCRIPTS_API_INTERNAL_ERROR: {
+    title: 'Internal error',
+    description: 'There was an internal error in the system.',
+  },
+  DBR_ERR_INTERNAL: {
+    title: 'Internal error',
+    description: 'There was an internal error in the system.',
+  },
+}
+
+export default (code: string): errorDescription =>
+  errorsExplanations[code] || { title: 'Unknown', description: '' }
