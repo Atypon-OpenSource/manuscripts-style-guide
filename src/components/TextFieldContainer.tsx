ed769111030fd3e957864616a6fb8bfce7570391
Teemu Koivisto <tkoivisto456@gmail.com>
2021-12-08 05:32:30 +0200

    (lint) run lint:fix

--- src/components/TextFieldContainer.tsx
+++ src/components/TextFieldContainer.tsx
@@ -26,29 +26,28 @@ interface TextFieldContainerProps {
   children: React.ReactElement<ErrorProps>
 }
 
-export const TextFieldContainer: React.FunctionComponent<
-  TextFieldContainerProps
-> = ({ label, error, children }) => {
-  const childrenWithErrorProp = React.Children.map(
-    children,
-    (child: React.ReactElement<ErrorProps>) =>
-      React.cloneElement(child, { error })
-  )
+export const TextFieldContainer: React.FunctionComponent<TextFieldContainerProps> =
+  ({ label, error, children }) => {
+    const childrenWithErrorProp = React.Children.map(
+      children,
+      (child: React.ReactElement<ErrorProps>) =>
+        React.cloneElement(child, { error })
+    )
 
-  return (
-    <React.Fragment>
-      {label ? (
-        <TextFieldLabel>
-          {label} {childrenWithErrorProp}
-        </TextFieldLabel>
-      ) : (
-        childrenWithErrorProp
-      )}
-      {error && (
-        <TextFieldError>
-          <TextFieldErrorItem>{error}</TextFieldErrorItem>
-        </TextFieldError>
-      )}
-    </React.Fragment>
-  )
-}
+    return (
+      <React.Fragment>
+        {label ? (
+          <TextFieldLabel>
+            {label} {childrenWithErrorProp}
+          </TextFieldLabel>
+        ) : (
+          childrenWithErrorProp
+        )}
+        {error && (
+          <TextFieldError>
+            <TextFieldErrorItem>{error}</TextFieldErrorItem>
+          </TextFieldError>
+        )}
+      </React.Fragment>
+    )
+  }
