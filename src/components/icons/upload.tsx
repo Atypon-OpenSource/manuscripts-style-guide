05ee2713c72d72ff7f6e42e0a376991836bbdb45
asouqi <asouqi@atypon.com>
2022-03-30 19:13:50 +0300

    EXT-020 - Implementation for UI Updates to use GraphQL attachments (LEAN-1019)

---
+++ src/components/icons/upload.tsx
@@ -0,0 +1,55 @@
+/*!
+ * © 2022 Atypon Systems LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React from 'react'
+
+const UploadIcon: React.FC<React.SVGAttributes<SVGElement>> = () => {
+  return (
+    <svg
+      width="17"
+      height="18"
+      viewBox="0 0 17 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8.5 12.5V1.5"
+        stroke="#6E6E6E"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M13.5 6.5L8.5 1.5L3.5 6.5"
+        stroke="#6E6E6E"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M15.5 16.5H1.5"
+        stroke="#6E6E6E"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+export default UploadIcon
