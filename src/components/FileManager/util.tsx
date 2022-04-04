6b2c3a0a63e30c465549fed4389cd8d8710cdc7d
Islam <imomani@atypon.com>
2022-04-04 16:21:21 +0300

    Images of inline files should not appear in the other files section

--- src/components/FileManager/util.tsx
+++ src/components/FileManager/util.tsx
@@ -73,8 +73,8 @@ export const designationWithFileSectionsMap = new Map<
   Designation,
   FileSectionType
 >([
-  [Designation.Figure, FileSectionType.OtherFile],
-  [Designation.Table, FileSectionType.OtherFile],
+  [Designation.Figure, FileSectionType.Inline],
+  [Designation.Table, FileSectionType.Inline],
 
   [Designation.Supplementary, FileSectionType.Supplements],
 
