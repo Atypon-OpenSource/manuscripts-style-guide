fe05e3dd191532b8728630cfa5cfa41f921f05ea
mbartenev-atypon <mbartenev@atypon.com>
2022-09-29 22:36:11 +0200

    LEAN-1622 moving OPS and Dashboard commond editor header components into styleguide

--- src/components/NavDropdown.tsx
+++ src/components/NavDropdown.tsx
@@ -159,7 +159,7 @@ export const NavDropdownButtonContainer = styled(SecondaryButton).attrs(
 `
 
 interface DropdownButtonProps {
-  as?: React.FunctionComponent<unknown>
+  as?: React.FunctionComponent<any>
   disabled?: boolean
   isOpen: boolean
   notificationsCount?: number
