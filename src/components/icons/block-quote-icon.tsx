d8f94905f85098d44f9605c7e779ee9b437da0a0
Mohammad Alnatsheh <mnatsheh@atypon.com>
2022-11-07 14:06:33 -0800

    LEAN-1342 - fix

--- src/components/icons/block-quote-icon.tsx
+++ src/components/icons/block-quote-icon.tsx
@@ -15,16 +15,13 @@
  */
 import React from 'react'
 
-import { IconProps } from './types'
-
-const BlockQuoteIcon: React.FC<IconProps> = (props: IconProps) => (
+const BlockQuoteIcon: React.FC<React.SVGAttributes<SVGElement>> = () => (
   <svg
     width="16"
     height="16"
     viewBox="0 0 16 16"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
-    {...props}
   >
     <path
       fillRule="evenodd"
