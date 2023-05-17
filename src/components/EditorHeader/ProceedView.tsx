91ed7d309c547d4fddd1588facfd21d685638af8
nghazzawi-atypon <nghazzawi@atypon.com>
2023-05-17 13:53:51 +0300

    LEAN-2192: LW - R8: Wrong confirmation message displays for Author proofing complete task popup

--- src/components/EditorHeader/ProceedView.tsx
+++ src/components/EditorHeader/ProceedView.tsx
@@ -124,7 +124,7 @@ export const ProceedView: React.FC<{
         : showComplete
         ? {
             header: 'Content reassigned successfully',
-            message: `to the ${nextStepType.label}`,
+            message: `to the ${currentStepType.label}`,
             actions: {
               primary: {
                 action: onCancelClick,
