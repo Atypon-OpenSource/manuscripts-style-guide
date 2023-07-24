c37cf6a358cb7cbd7133dd6c560859b9e03bf88e
Mohammad Betamony <112621558+mbetamony@users.noreply.github.com>
2023-07-24 17:16:56 +0300

    LEAN-2501 (#100)
    * cleanup schemas, remove sessionID, _rev
    
    * add test version
    
    * Add final versions for merging

--- stories/data/notes.ts
+++ stories/data/notes.ts
@@ -27,7 +27,6 @@ export const notes: ManuscriptNote[] = [
       '<div><blockquote>some quoted text</blockquote><p>This is a <span class="keyword" data-keyword="keyword-1">#note</span> for <span class="user" data-user="user-2">@test</span>.</p></div>',
     createdAt: Math.floor(new Date('2018-01-22T08:00:00Z').getTime() / 1000),
     updatedAt: Math.floor(new Date('2018-01-23T08:00:00Z').getTime() / 1000),
-    sessionID: 'foo',
     originalText: '',
     source: 'EDITOR',
     contributions: [
