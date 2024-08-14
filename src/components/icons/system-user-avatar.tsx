b9653be19aea8edfae650c9cb3366ef64769d366
Noura Ghazzawi <nghazzawi@atypon.com>
2024-08-14 11:53:21 +0300

    add props

--- src/components/icons/system-user-avatar.tsx
+++ src/components/icons/system-user-avatar.tsx
@@ -24,6 +24,7 @@ const SystemUserAvatarIcon: React.FC<IconProps> = (props) => (
     viewBox="0 0 44 44"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    {...props}
   >
     <path
       fillRule="evenodd"
