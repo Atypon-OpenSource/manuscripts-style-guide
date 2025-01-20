49174ed638b3153c81488357c5ba9c0fa190df32
Fahid Alwaisi <fwaisi@atypon.com>
2025-01-20 14:54:48 +0300

    LEAN-4213 AUR-060 - Author & Affiliation - Manage Affiliations (#249)
    Co-authored-by: zax-assistant[bot] <103364958+zax-assistant[bot]@users.noreply.github.com>

---
+++ src/components/icons/add-institution.tsx
@@ -0,0 +1,95 @@
+/*!
+ * © 2025 Atypon Systems LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React from 'react'
+
+import { IconProps } from './types'
+
+const AddInstitutionIcon: React.FC<IconProps> = (props) => (
+  <svg
+    width="16"
+    height="16"
+    viewBox="0 0 16 16"
+    fill="none"
+    {...props}
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clipPath="url(#clip0_23584_38259)">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M1 5.5C0.7 5.5 0.5 5.3 0.5 5V4C0.5 3.8 0.6 3.6 0.8 3.55L8.1 0.55C8.2 0.5 8.25 0.5 8.3 0.5C8.35 0.5 8.45 0.5 8.5 0.55L15.2 3.55C15.4 3.6 15.5 3.8 15.5 4V5C15.5 5.3 15.3 5.5 15 5.5H1Z"
+        fill="white"
+      />
+      <path
+        d="M8.3 1L15 4V5H1V4L8.3 1ZM8.3 0C8.15 0 8.05 0 7.9 0.05L0.6 3.05C0.25 3.25 0 3.6 0 4V5C0 5.55 0.45 6 1 6H15C15.55 6 16 5.55 16 5V4C16 3.6 15.75 3.25 15.4 3.1L8.7 0.1C8.6 0.05 8.45 0 8.3 0Z"
+        fill="#0D79D0"
+      />
+      <path
+        d="M7.9999 4.10039C8.60742 4.10039 9.0999 3.6079 9.0999 3.00039C9.0999 2.39288 8.60742 1.90039 7.9999 1.90039C7.39239 1.90039 6.8999 2.39288 6.8999 3.00039C6.8999 3.6079 7.39239 4.10039 7.9999 4.10039Z"
+        fill="#0D79D0"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M7.5 13.5C7.2 13.5 7 13.3 7 13V6C7 5.7 7.2 5.5 7.5 5.5H8.5C8.8 5.5 9 5.7 9 6V13C9 13.3 8.8 13.5 8.5 13.5H7.5Z"
+        fill="white"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M2 13.5C1.7 13.5 1.5 13.3 1.5 13V6C1.5 5.7 1.7 5.5 2 5.5H3C3.3 5.5 3.5 5.7 3.5 6V13C3.5 13.3 3.3 13.5 3 13.5H2Z"
+        fill="white"
+      />
+      <path
+        d="M3 6V13H2V6H3ZM3 5H2C1.45 5 1 5.45 1 6V13C1 13.55 1.45 14 2 14H3C3.55 14 4 13.55 4 13V6C4 5.45 3.55 5 3 5Z"
+        fill="#0D79D0"
+      />
+      <path
+        d="M13.75 13.75V15.25C13.75 15.6642 13.4142 16 13 16C12.5858 16 12.25 15.6642 12.25 15.25V13.75H10.75C10.3358 13.75 10 13.4142 10 13C10 12.5858 10.3358 12.25 10.75 12.25H12.25V10.75C12.25 10.3358 12.5858 10 13 10C13.4142 10 13.75 10.3358 13.75 10.75V12.25H15.25C15.6642 12.25 16 12.5858 16 13C16 13.4142 15.6642 13.75 15.25 13.75H13.75Z"
+        fill="#0D79D0"
+      />
+      <path
+        d="M13.75 13.75V15.25C13.75 15.6642 13.4142 16 13 16C12.5858 16 12.25 15.6642 12.25 15.25V13.75H10.75C10.3358 13.75 10 13.4142 10 13C10 12.5858 10.3358 12.25 10.75 12.25H12.25V10.75C12.25 10.3358 12.5858 10 13 10C13.4142 10 13.75 10.3358 13.75 10.75V12.25H15.25C15.6642 12.25 16 12.5858 16 13C16 13.4142 15.6642 13.75 15.25 13.75H13.75Z"
+        stroke="#0D79D0"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M0 14C0 13.4477 0.447715 13 1 13H8.94186C9.218 13 9.44186 13.2239 9.44186 13.5C9.44186 13.7761 9.218 14 8.94186 14H1V15H8.94186C9.218 15 9.44186 15.2239 9.44186 15.5C9.44186 15.7761 9.218 16 8.94186 16H1C0.447714 16 0 15.5523 0 15V14Z"
+        fill="#0D79D0"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M14 9.25L14 6L13 6L13 9.25C13 9.52614 12.7761 9.75 12.5 9.75C12.2239 9.75 12 9.52614 12 9.25L12 6C12 5.44771 12.4477 5 13 5L14 5C14.5523 5 15 5.44772 15 6L15 9.25C15 9.52614 14.7761 9.75 14.5 9.75C14.2239 9.75 14 9.52614 14 9.25Z"
+        fill="#0D79D0"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M8.5 11.25L8.5 6L7.5 6L7.5 13.25C7.5 13.5261 7.27614 13.75 7 13.75C6.72386 13.75 6.5 13.5261 6.5 13.25L6.5 6C6.5 5.44771 6.94772 5 7.5 5L8.5 5C9.05228 5 9.5 5.44772 9.5 6L9.5 11.25C9.5 11.5261 9.27614 11.75 9 11.75C8.72386 11.75 8.5 11.5261 8.5 11.25Z"
+        fill="#0D79D0"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_23584_38259">
+        <rect width="16" height="16" fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+)
+
+export default AddInstitutionIcon
