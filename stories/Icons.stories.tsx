95142a3177e9217a1b6cafce687c3bffd7d4c540
Noura Ghazzawi <nghazzawi@atypon.com>
2025-11-10 14:22:15 +0300

    LEAN-5086

--- stories/Icons.stories.tsx
+++ stories/Icons.stories.tsx
@@ -94,6 +94,7 @@ const iconList: { name: string; Component: React.FC<IconProps> }[] = [
   { name: 'ImageLeftIcon', Component: Icons.ImageLeftIcon },
   { name: 'ImageDefaultIcon', Component: Icons.ImageDefaultIcon },
   { name: 'ImageRightIcon', Component: Icons.ImageRightIcon },
+  { name: 'InspectorPluginIcon', Component: Icons.InspectorPluginIcon },
   { name: 'LinkIcon', Component: Icons.LinkIcon },
   { name: 'LogoutIcon', Component: Icons.LogoutIcon },
   { name: 'ManuscriptIcon', Component: Icons.ManuscriptIcon },
