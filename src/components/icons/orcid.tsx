a49add1de8341f39c72ab190b107cc94b81ec5db
mbartenev-atypon <mbartenev@atypon.com>
2026-03-09 11:42:11 +0100

    type fixes, icon viewPort added

--- src/components/icons/orcid.tsx
+++ src/components/icons/orcid.tsx
@@ -18,10 +18,11 @@ import React from 'react'
 
 import { IconProps } from './types'
 
-const ORCIDIcon: React.FC<IconProps> = (props: IconProps) => (
+const ORCIDIcon: React.FC<IconProps> = (props) => (
   <svg
     width="32"
     height="32"
+    viewBox="0 0 32 32"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
