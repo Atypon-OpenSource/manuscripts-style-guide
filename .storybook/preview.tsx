9d064ab345cf8c3498f81673f011fd3f145f497d
mbartenev-atypon <mbartenev@atypon.com>
2026-05-13 18:56:14 +0200

    upped mostly all

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 import { ThemeProvider } from 'styled-components'
-import 'typeface-lato'
+import '@fontsource/lato'
 import { defaultTheme } from '../src/defaultTheme'
 
 const preview: Preview = {
